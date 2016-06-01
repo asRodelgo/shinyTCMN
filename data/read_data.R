79155ad6c787a016662a1d9200309a52c47fcacd
Alberto Sanchez <alberto.sanchez.rodelgo@gmail.com>
2016-06-01 10:04:21 -0400

    Use absolute path to externalized data files

--- data/read_data.R
+++ data/read_data.R
@@ -7,13 +7,13 @@ options(scipen=999) ## Prevent scientific notation
 # big data file ----------------------------
 
 # fast read
-#TCMN_data <- fread("data/TCMN_data.csv")
+#TCMN_data <- fread("/srv/shiny-server/shinyTCMN-data/data/TCMN_data.csv")
 #TCMN_data <- as.data.frame(TCMN_data, stringsAsFactors=FALSE)
 # normal speed read
-TCMN_data <- read.csv("data/TCMN_data.csv", colClasses = c(rep("character",4),rep("numeric",2),rep("character",2)))
+TCMN_data <- read.csv("/srv/shiny-server/shinyTCMN-data/data/TCMN_data.csv", colClasses = c(rep("character",4),rep("numeric",2),rep("character",2)))
 
 # country table ----------------------------
-countries <- read.csv("data/CountryClassification.csv", stringsAsFactors = FALSE)
+countries <- read.csv("/srv/shiny-server/shinyTCMN-data/data/CountryClassification.csv", stringsAsFactors = FALSE)
 # Avoid ISO2 code for Namibia be confused by NA
 countries[countries$CountryCodeISO3=="NAM",]$CountryCodeISO2 <- "NA"
 countries <- arrange(countries, Country)
@@ -26,45 +26,45 @@ countryDeps <- filter(countries, !(CMU==""))
 countryDeps <- arrange(select(countryDeps, CountryCodeISO3, RegionCodeALL, Region ,CMU), CMU)
 
 # country Coordinates --------------
-countryCoords <- read.csv("data/countryCoords.csv", stringsAsFactors = FALSE)
+countryCoords <- read.csv("/srv/shiny-server/shinyTCMN-data/data/countryCoords.csv", stringsAsFactors = FALSE)
 
 # indicator table ----------------------------
-indicators <- read.csv("data/IndicatorClassification.csv", stringsAsFactors = FALSE)
+indicators <- read.csv("/srv/shiny-server/shinyTCMN-data/data/IndicatorClassification.csv", stringsAsFactors = FALSE)
 
 # TCMN specific source ----------------------------
-TCMN_sources <- read.csv("data/TCMN_sources.csv", stringsAsFactors = FALSE)
+TCMN_sources <- read.csv("/srv/shiny-server/shinyTCMN-data/data/TCMN_sources.csv", stringsAsFactors = FALSE)
 
 # TCMN specific indicators ----------------------------
-TCMN_indic <- read.csv("data/TCMN_Indicators.csv", stringsAsFactors = FALSE)
+TCMN_indic <- read.csv("/srv/shiny-server/shinyTCMN-data/data/TCMN_Indicators.csv", stringsAsFactors = FALSE)
 
 # TCMN specific datasets ----------------------------
-TCMN_datasets <- read.csv("data/TCMN_datasets.csv", stringsAsFactors = FALSE)
+TCMN_datasets <- read.csv("/srv/shiny-server/shinyTCMN-data/data/TCMN_datasets.csv", stringsAsFactors = FALSE)
 
 # WITS Imports ----------------------------
-mWits <- read.csv("data/mWits.csv", colClasses = c(rep("character",3),rep("numeric",2),rep("character",2)))
+mWits <- read.csv("/srv/shiny-server/shinyTCMN-data/data/mWits.csv", colClasses = c(rep("character",3),rep("numeric",2),rep("character",2)))
 
 # WITS Exports ----------------------------
-xWits <- read.csv("data/xWits.csv", colClasses = c(rep("character",3),rep("numeric",2),rep("character",2)))
+xWits <- read.csv("/srv/shiny-server/shinyTCMN-data/data/xWits.csv", colClasses = c(rep("character",3),rep("numeric",2),rep("character",2)))
 
 # IBRD T&C projects portfolio --------------
-# TCprojects1 <- read.csv("/Users/asanchez3/shinyTCMN/data/TCprojects1.csv", stringsAsFactors = FALSE)
-# TCprojects2 <- read.csv("/Users/asanchez3/shinyTCMN/data/TCprojects2.csv", stringsAsFactors = FALSE)
+# TCprojects1 <- read.csv("/Users/asanchez3/shinyTCMN//srv/shiny-server/shinyTCMN-/srv/shiny-server/shinyTCMN-data/data//srv/shiny-server/shinyTCMN-data/data/TCprojects1.csv", stringsAsFactors = FALSE)
+# TCprojects2 <- read.csv("/Users/asanchez3/shinyTCMN//srv/shiny-server/shinyTCMN-data/data/TCprojects2.csv", stringsAsFactors = FALSE)
 # TCprojects <- rbind(TCprojects1,TCprojects2)
                                        
 TCprojectList <- list()
 pieces <- 5 # number of TC data frames
 for (i in 1:pieces){
-  TCprojectList[[i]] <- read.csv(paste0("/Users/asanchez3/shinyTCMN/data/TCprojects",i,".csv"),stringsAsFactors = FALSE)
+  TCprojectList[[i]] <- read.csv(paste0("/srv/shiny-server/shinyTCMN-data/data/TCprojects",i,".csv"),stringsAsFactors = FALSE)
   TCprojectList[[i]] <- select(TCprojectList[[i]], -upi_nbr_c)
 }
 TCprojects <- bind_rows(TCprojectList)
                       
 # IFC projects portfolio --------------
-IFCprojects <- read.csv("data/IFCprojects.csv", stringsAsFactors = FALSE)
+IFCprojects <- read.csv("/srv/shiny-server/shinyTCMN-data/data/IFCprojects.csv", stringsAsFactors = FALSE)
 
 # SCD/CPF most recent --------------
-mostRecentDocs <- read.csv("data/SCDCPFdocuments.csv", stringsAsFactors = FALSE)
+mostRecentDocs <- read.csv("/srv/shiny-server/shinyTCMN-data/data/SCDCPFdocuments.csv", stringsAsFactors = FALSE)
 
 # SCD/CPF planned --------------
-plannedDocs <- read.csv("data/Planneddocuments.csv", stringsAsFactors = FALSE)
+plannedDocs <- read.csv("/srv/shiny-server/shinyTCMN-data/data/Planneddocuments.csv", stringsAsFactors = FALSE)
 
